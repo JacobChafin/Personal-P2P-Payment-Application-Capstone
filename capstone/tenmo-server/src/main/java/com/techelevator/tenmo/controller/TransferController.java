fc5ff1e78db65e2b849bd2e8b07ac2532460a965
Dan Gregg <danggregg@gmail.com>
2022-03-11 12:07:18 -0500

    Transfer and Account Controller

---
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -0,0 +1,33 @@
+package com.techelevator.tenmo.controller;
+
+
+import com.techelevator.tenmo.dao.UserDao;
+import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.tenmo.model.User;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@PreAuthorize("isAuthenticated()")
+public class TransferController {
+    private UserDao userDao;
+    private Transfer transfer;
+
+
+    @RequestMapping(path = "transfers/id", method = RequestMethod.GET)
+    public Transfer getTransfer(@PathVariable int userid) {
+//        Transfer transfer = transfer.getTransferId(userid);
+        return transfer;
+    }
+
+    @RequestMapping(path = "account/transfers/id", method = RequestMethod.GET)
+    public List<Transfer> listOfTransfers (@PathVariable int userid) {
+        List<User> response = userDao.findAll();
+        return null;
+    }
+}
