721403556c2fbe922c27f5ec68d92302edefc5bd
Dan Gregg <danggregg@gmail.com>
2022-03-12 15:49:20 -0500

    Transfer Server

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -3,10 +3,7 @@ package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.AccountDao;
 import com.techelevator.tenmo.dao.JdbcAccountDao;
-<<<<<<< HEAD
-=======
 import com.techelevator.tenmo.dao.JdbcUserDao;
->>>>>>> e4d86f6895b7e5f2347280412d8dc3a715fc37bd
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.User;
@@ -23,7 +20,6 @@ import java.util.List;
 public class AccountController {
 private UserDao userDao;
 private JdbcAccountDao accountDao;
-private UserDao userDao;
 
     public AccountController(JdbcAccountDao account, JdbcUserDao user) {
         this.accountDao = account;
--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -13,32 +13,32 @@ public class JdbcTransferDao implements TransferDao{
     private JdbcTemplate jdbcTemplate;
 
 
-    @Override
-    public Transfer getTransfersById (int transferId) {
-        Transfer transfer = new Transfer();
-        String sql = "SELECT * " +
-                "FROM transfer" +
-                "WHERE transfer_id = ?";
-
-        SqlRowSet transferList = jdbcTemplate.queryForRowSet(sql, transferId);
-        if (transferList.next()) {
-            transfer = mapRowToTransfer(transferList);
-
-        }
-        return transfer;
-    }
-
-    @Override
-    public List<Transfer> getListOfAllTransfers(int userId) {
-        List<Transfer> listOfTransfers = new ArrayList<>();
-
-        String sql = "SELECT *" +
-                "FROM transfer" +
-                "JOIN account ON account.account_id = transfer.account_from" +
-                "WHERE account.account_id = ?";
-
-            // TODO Map ROW Set for transfer is in USER
-        }
+//    @Override
+//    public Transfer getTransfersById (int transferId) {
+//        Transfer transfer = new Transfer();
+//        String sql = "SELECT * " +
+//                "FROM transfer" +
+//                "WHERE transfer_id = ?";
+//
+//        SqlRowSet transferList = jdbcTemplate.queryForRowSet(sql, transferId);
+//        if (transferList.next()) {
+////            transfer = mapRowToTransfer(transferList);
+//
+//        }
+//        return transfer;
+//    }
+//
+//    @Override
+//    public List<Transfer> getListOfAllTransfers(int userId) {
+//        List<Transfer> listOfTransfers = new ArrayList<>();
+//
+//        String sql = "SELECT *" +
+//                "FROM transfer" +
+//                "JOIN account ON account.account_id = transfer.account_from" +
+//                "WHERE account.account_id = ?";
+//
+//            // TODO Map ROW Set for transfer is in USER
+//        }
 
 
 
