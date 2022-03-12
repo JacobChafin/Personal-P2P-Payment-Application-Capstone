8219738eeadfd5295bcee4b7024277403ee98056
Dan Gregg <danggregg@gmail.com>
2022-03-12 15:58:30 -0500

    Updated Transfer Server POST meltdown

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcUserDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcUserDao.java
@@ -189,23 +189,23 @@ public class JdbcUserDao implements UserDao {
     // TODO Be able to retrieve all transfer information based on transfer ID
 
 
-    private Transfer mapRowToTransfer(SqlRowSet stupidDumbSqlRowSetResults) {
-
-
-        Transfer transfer = new Transfer();
-        transfer.setAccountFrom(stupidDumbSqlRowSetResults.getInt("account_from"));
-        transfer.setAccountTo(stupidDumbSqlRowSetResults.getInt("account_to"));
-        transfer.setTransferId(stupidDumbSqlRowSetResults.getInt("transfer_id"));
-        transfer.setTransferStatusId(stupidDumbSqlRowSetResults.getInt("transfer_status_id"));
-        transfer.setTransferTypeId(stupidDumbSqlRowSetResults.getInt("transfer_type_id"));
-        transfer.setTransferAmount(stupidDumbSqlRowSetResults.getInt("amount"));
-
-        transfer.setTransferType(stupidDumbSqlRowSetResults.getString("transfer_type_desc"));
-        transfer.setTransferStatus(stupidDumbSqlRowSetResults.getString("transfer_status_desc"));
-//        transfer.setUserFrom(stupidDumbSqlRowSetResults.getString("user_from"));
-//        transfer.setUserTo(stupidDumbSqlRowSetResults.getString("user_to"));
-        return transfer;
-    }
+//    private Transfer mapRowToTransfer(SqlRowSet stupidDumbSqlRowSetResults) {
+//
+//
+//        Transfer transfer = new Transfer();
+//        transfer.setAccountFrom(stupidDumbSqlRowSetResults.getInt("account_from"));
+//        transfer.setAccountTo(stupidDumbSqlRowSetResults.getInt("account_to"));
+//        transfer.setTransferId(stupidDumbSqlRowSetResults.getInt("transfer_id"));
+//        transfer.setTransferStatusId(stupidDumbSqlRowSetResults.getInt("transfer_status_id"));
+//        transfer.setTransferTypeId(stupidDumbSqlRowSetResults.getInt("transfer_type_id"));
+//        transfer.setTransferAmount(stupidDumbSqlRowSetResults.getInt("amount"));
+//
+//        transfer.setTransferType(stupidDumbSqlRowSetResults.getString("transfer_type_desc"));
+//        transfer.setTransferStatus(stupidDumbSqlRowSetResults.getString("transfer_status_desc"));
+////        transfer.setUserFrom(stupidDumbSqlRowSetResults.getString("user_from"));
+////        transfer.setUserTo(stupidDumbSqlRowSetResults.getString("user_to"));
+//        return transfer;
+//    }
 
 
 
