94f282ba88d6ead287ee221cdf36202aece24d0e
Dan Gregg <danggregg@gmail.com>
2022-03-13 14:43:28 -0400

    server housekeeping

--- capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -1,18 +1,15 @@
 package com.techelevator.tenmo.services;
 
+import com.techelevator.tenmo.model.AuthenticatedUser;
 import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.util.BasicLogger;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
-
-import com.techelevator.tenmo.model.AuthenticatedUser;
-import com.techelevator.tenmo.model.UserCredentials;
 
 public class TransferService {
     private static final String API_BASE_URL = "http://localhost:8080/";
